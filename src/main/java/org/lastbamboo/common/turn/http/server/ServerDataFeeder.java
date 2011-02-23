f79bf2e00544232b5f487e21e37191ee82e66252
Adam Fisk <a@littleshoot.org>
2011-02-22 18:04:49 -0800

    Formatting and build fixes

--- src/main/java/org/lastbamboo/common/turn/http/server/ServerDataFeeder.java
+++ src/main/java/org/lastbamboo/common/turn/http/server/ServerDataFeeder.java
@@ -26,154 +26,131 @@ import org.slf4j.LoggerFactory;
  * Class that feeds data received from the TURN server to a locally-running
  * HTTP server.
  */
-public class ServerDataFeeder implements TurnClientListener, IoServiceListener
-    {
+public class ServerDataFeeder implements TurnClientListener, IoServiceListener {
 
     private final Logger m_log = LoggerFactory.getLogger(getClass());
-    
-    private final Map<InetSocketAddress, IoSession> m_addressesToSessions =
+
+    private final Map<InetSocketAddress, IoSession> m_addressesToSessions = 
         new ConcurrentHashMap<InetSocketAddress, IoSession>();
 
     private final InetSocketAddress m_serverAddress;
-    
+
     /**
-     * Creates a new {@link ServerDataFeeder} for feeding data to a local 
-     * HTTP server.
+     * Creates a new {@link ServerDataFeeder} for feeding data to a local HTTP
+     * server.
      * 
      * @param serverAddress The address of the server to connect to.
      */
-    public ServerDataFeeder(final InetSocketAddress serverAddress)
-        {
+    public ServerDataFeeder(final InetSocketAddress serverAddress) {
         m_serverAddress = serverAddress;
-        }
-    
-    public void onData(final InetSocketAddress remoteAddress, 
-        final IoSession session, final byte[] data)
-        {
+    }
+
+    public void onData(final InetSocketAddress remoteAddress,
+            final IoSession session, final byte[] data) {
         m_log.debug("Received data message");
-        final IoSession localSession = 
-            onRemoteAddressOpened(remoteAddress, session);
+        final IoSession localSession = onRemoteAddressOpened(remoteAddress,
+                session);
         final ByteBuffer dataBuf = ByteBuffer.wrap(data);
-        
+
         localSession.write(dataBuf);
         m_log.debug("Local bytes written: {}", localSession.getWrittenBytes());
-        }
+    }
 
     public IoSession onRemoteAddressOpened(
-        final InetSocketAddress remoteAddress, final IoSession ioSession)
-        {
+            final InetSocketAddress remoteAddress, final IoSession ioSession) {
         // We don't synchronize here because we're processing data from
         // a single TCP connection.
-        if (m_addressesToSessions.containsKey(remoteAddress))
-            {
-            m_log.debug("Using existing local connection to: {}",remoteAddress);
-            // This is the connection from the local proxy server to the 
-            // local client.  So we're writing to our local server.
+        if (m_addressesToSessions.containsKey(remoteAddress)) {
+            m_log.debug("Using existing local connection to: {}", remoteAddress);
+            // This is the connection from the local proxy server to the
+            // local client. So we're writing to our local server.
             return m_addressesToSessions.get(remoteAddress);
-            }
-        else
-            {
-            m_log.debug("Opening new local socket for remote address: {}", 
-                remoteAddress);
+        } else {
+            m_log.debug("Opening new local socket for remote address: {}",
+                    remoteAddress);
             final IoConnector connector = new SocketConnector();
             connector.addListener(this);
-            final ThreadModel threadModel = 
-                ExecutorThreadModel.getInstance("TCP-TURN-Local-Socket");
+            final ThreadModel threadModel = ExecutorThreadModel
+                    .getInstance("TCP-TURN-Local-Socket");
             connector.getDefaultConfig().setThreadModel(threadModel);
-            //connector.getDefaultConfig().setThreadModel(ThreadModel.MANUAL);
-            final IoHandler ioHandler = 
-                new TurnLocalIoHandler(ioSession, remoteAddress);
-            
-            final ConnectFuture ioFuture = 
-                connector.connect(this.m_serverAddress, ioHandler);
-            
-            // We're just connecting locally, so it should be much quicker 
+            // connector.getDefaultConfig().setThreadModel(ThreadModel.MANUAL);
+            final IoHandler ioHandler = new TurnLocalIoHandler(ioSession,
+                    remoteAddress);
+
+            final ConnectFuture ioFuture = connector.connect(
+                    this.m_serverAddress, ioHandler);
+
+            // We're just connecting locally, so it should be much quicker
             // than this unless there's something wrong.
             ioFuture.join(10 * 1000);
             final IoSession session = ioFuture.getSession();
-            if (session == null || !session.isConnected())
-                {
-                m_log.error("Could not connect to server: {}", 
-                    this.m_serverAddress);
+            if (session == null || !session.isConnected()) {
+                m_log.error("Could not connect to server: {}",
+                        this.m_serverAddress);
                 return null;
-                }
-            else
-                {
+            } else {
                 m_log.debug("Connected to server: {}", this.m_serverAddress);
                 this.m_addressesToSessions.put(remoteAddress, session);
                 return session;
-                }
             }
         }
-    
+    }
 
-    public void onRemoteAddressClosed(final InetSocketAddress remoteAddress)
-        {
-        if (!this.m_addressesToSessions.containsKey(remoteAddress))
-            {
+    public void onRemoteAddressClosed(final InetSocketAddress remoteAddress) {
+        if (!this.m_addressesToSessions.containsKey(remoteAddress)) {
             // This would be odd -- could indicate someone fiddling
             // with our servers?
             m_log.warn("We don't know about the remote address: {}",
-                remoteAddress);
+                    remoteAddress);
             m_log.warn("Address not in: {}", m_addressesToSessions.keySet());
-            }
-        else
-            {
+        } else {
             m_log.debug("Closing connection to local HTTP server...");
-            final IoSession session = 
-                this.m_addressesToSessions.remove(remoteAddress);
-            
-            // Stop the local session.  In particular, it the session
+            final IoSession session = this.m_addressesToSessions
+                    .remove(remoteAddress);
+
+            // Stop the local session. In particular, it the session
             // is in the middle of an HTTP transfer, this will stop
             // the HTTP server from sending more data to a host that's
             // no longer there on the other end.
             session.close();
-            }
         }
+    }
 
-    public void serviceActivated(final IoService service, 
-        final SocketAddress serviceAddress, final IoHandler handler, 
-        final IoServiceConfig config)
-        {
-        }
+    public void serviceActivated(final IoService service,
+            final SocketAddress serviceAddress, final IoHandler handler,
+            final IoServiceConfig config) {
+    }
 
-    public void serviceDeactivated(final IoService service, 
-        final SocketAddress serviceAddress, final IoHandler handler, 
-        final IoServiceConfig config)
-        {
-        }
+    public void serviceDeactivated(final IoService service,
+            final SocketAddress serviceAddress, final IoHandler handler,
+            final IoServiceConfig config) {
+    }
 
-    public void sessionCreated(final IoSession session)
-        {
-        }
+    public void sessionCreated(final IoSession session) {
+    }
 
-    public void sessionDestroyed(final IoSession session)
-        {
+    public void sessionDestroyed(final IoSession session) {
         // Inefficient, but not much to do.
         for (final Map.Entry<InetSocketAddress, IoSession> entry : 
-            this.m_addressesToSessions.entrySet())
-            {
-            if (entry.getValue().equals(session))
-                {
+                this.m_addressesToSessions.entrySet()) {
+            if (entry.getValue().equals(session)) {
                 final InetSocketAddress key = entry.getKey();
                 m_log.debug("Removing session for address: {}", key);
                 this.m_addressesToSessions.remove(key);
                 return;
-                }
             }
-        m_log.warn("Did not find session:\n{}\nin:{}", 
-            session, m_addressesToSessions);
         }
+        m_log.warn("Did not find session:\n{}\nin:{}", session,
+                m_addressesToSessions);
+    }
 
-    public void close()
-        {
+    public void close() {
         // Now close any of the local "proxied" sockets as well.
         final Collection<IoSession> sessions = 
             this.m_addressesToSessions.values();
-        for (final IoSession curSession : sessions)
-            {
+        for (final IoSession curSession : sessions) {
             curSession.close();
-            }
-        this.m_addressesToSessions.clear();
         }
+        this.m_addressesToSessions.clear();
     }
+}
--- src/test/java/org/lastbamboo/common/turn/http/server/ServerDataFeederTest.java
+++ src/test/java/org/lastbamboo/common/turn/http/server/ServerDataFeederTest.java
@@ -28,24 +28,24 @@ import org.apache.commons.lang.ArrayUtils;
 import org.littleshoot.mina.common.IoSession;
 import org.littleshoot.mina.common.WriteFuture;
 import org.junit.Test;
-import org.lastbamboo.common.stun.stack.message.turn.SendIndication;
+import org.littleshoot.stun.stack.message.turn.SendIndication;
 import org.lastbamboo.common.turn.http.server.stubs.IoSessionStub;
 import org.littleshoot.util.ByteBufferUtils;
 import org.littleshoot.util.NetworkUtils;
+import org.littleshoot.util.ThreadUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Test for the class that feeds data to a server by opening new connections. 
  */
-public class ServerDataFeederTest
-    {
-    
+public class ServerDataFeederTest {
+
     private final Logger m_log = LoggerFactory.getLogger(getClass());
-    
+
     private ServerSocket m_dataServerSocket;
 
-    private final AtomicBoolean m_unexpectedDataOnServer =
+    private final AtomicBoolean m_unexpectedDataOnServer = 
         new AtomicBoolean(false);
 
     private final AtomicInteger m_dataServerMessagesReceived = 
@@ -53,20 +53,19 @@ public class ServerDataFeederTest
 
     private volatile int m_dataServerMessagesValidated = 0;
 
-    private final AtomicBoolean m_socketServingFailed = 
-        new AtomicBoolean(false);
+    private final AtomicBoolean m_socketServingFailed = new AtomicBoolean(false);
 
     private final AtomicInteger m_dataServerSockets = new AtomicInteger(0);
-    
+
     private final AtomicBoolean m_dataServerStarted = new AtomicBoolean(false);
     private final AtomicBoolean m_stopDataServer = new AtomicBoolean(false);
-    
-    private final AtomicReference<byte[]> m_dataOnServer =
+
+    private final AtomicReference<byte[]> m_dataOnServer = 
         new AtomicReference<byte[]>();
-    
-    private final AtomicInteger m_dataMessagesWrittenFromServer =
+
+    private final AtomicInteger m_dataMessagesWrittenFromServer = 
         new AtomicInteger(0);
-    
+
     private static final byte[] DATA = createData();
 
     // We limit this data size because most protocols passing data
@@ -77,274 +76,231 @@ public class ServerDataFeederTest
 
     private static final int NUM_MESSAGES_PER_HOST = 10;
 
-    @Test public void testFeedServer() throws Exception
-        {
+    @Test
+    public void testFeedServer() throws Exception {
         final InetSocketAddress serverAddress = startThreadedServer();
-        synchronized (m_dataServerStarted)
-            {
-            if (!m_dataServerStarted.get())
-                {
+        synchronized (m_dataServerStarted) {
+            if (!m_dataServerStarted.get()) {
                 m_dataServerStarted.wait(3000);
-                }
             }
+        }
         final ServerDataFeeder feeder = new ServerDataFeeder(serverAddress);
-        
-        final Map<InetSocketAddress, Collection<ByteBuffer>>  addressesToMessages = 
+
+        final Map<InetSocketAddress, Collection<ByteBuffer>> addressesToMessages = 
             new HashMap<InetSocketAddress, Collection<ByteBuffer>>();
         final Collection<SendIndication> messages = 
             new LinkedList<SendIndication>();
-        
+
         final AtomicInteger totalDataSent = new AtomicInteger(0);
         final AtomicInteger totalDataReceived = new AtomicInteger(0);
-        final IoSession session = new IoSessionStub()
-            {
+        final IoSession session = new IoSessionStub() {
             @Override
-            public WriteFuture write(final Object message)
-                {
-                final SendIndication di = (SendIndication) message;
-                final InetSocketAddress address = di.getRemoteAddress();
-                if (addressesToMessages.containsKey(address))
-                    {
+            public WriteFuture write(final Object message) {
+                final SendIndication si = (SendIndication) message;
+                final InetSocketAddress address = si.getRemoteAddress();
+                if (addressesToMessages.containsKey(address)) {
                     final Collection<ByteBuffer> bufs = 
                         addressesToMessages.get(address);
-                    addRaw(bufs, di);
-                    }
-                else
-                    {
+                    addRaw(bufs, si);
+                } else {
                     final Collection<ByteBuffer> bufs = 
                         new LinkedList<ByteBuffer>();
                     addressesToMessages.put(address, bufs);
-                    addRaw(bufs, di);
-                    }
-                messages.add(di);
-                if (messages.size() == NUM_REMOTE_HOSTS)
-                    {
-                    synchronized (messages)
-                        {
-                        messages.notify();
-                        }
-                    }
-                return null;
+                    addRaw(bufs, si);
                 }
+                messages.add(si);
+                if (messages.size() == NUM_REMOTE_HOSTS) {
+                    synchronized (messages) {
+                        messages.notify();
+                    }
+                }
+                return null;
+            }
 
-            // This method is required to extract the data from the TCP frames 
+            // This method is required to extract the data from the TCP frames
             // to verify it matches the original data sent (since the server
             // just echoes everything it sees).
-            private void addRaw(final Collection<ByteBuffer> bufs, 
-                final SendIndication si)
-                {
+            private void addRaw(final Collection<ByteBuffer> bufs,
+                    final SendIndication si) {
                 final byte[] rawData = si.getData();
-                final byte[] noFrame = 
-                    ArrayUtils.subarray(rawData, 2, rawData.length);
                 
-                totalDataReceived.addAndGet(noFrame.length);
-                bufs.add(ByteBuffer.wrap(noFrame));
-                
-                
-                synchronized (totalDataReceived)
-                    {
-                    if (totalDataReceived.get() == totalDataSent.get())
-                        {
+                // We no longer use TcpFrames, so the below is not necessary.
+                //final byte[] noFrame = ArrayUtils.subarray(rawData, 2,
+                //        rawData.length);
+
+                totalDataReceived.addAndGet(rawData.length);
+                bufs.add(ByteBuffer.wrap(rawData));
+
+                synchronized (totalDataReceived) {
+                    if (totalDataReceived.get() == totalDataSent.get()) {
                         totalDataReceived.notify();
-                        }
                     }
                 }
-            };
-        
-        for (int j = 0; j < NUM_MESSAGES_PER_HOST; j++)
-            {
-            for (int i = 0; i < NUM_REMOTE_HOSTS; i++)
-                {
-                final InetSocketAddress remoteAddress = 
-                    new InetSocketAddress("44.52.67."+(1+i), 4728+i);
+            }
+        };
+
+        for (int j = 0; j < NUM_MESSAGES_PER_HOST; j++) {
+            for (int i = 0; i < NUM_REMOTE_HOSTS; i++) {
+                final InetSocketAddress remoteAddress = new InetSocketAddress(
+                        "44.52.67." + (1 + i), 4728 + i);
                 feeder.onRemoteAddressOpened(remoteAddress, session);
                 feeder.onData(remoteAddress, session, DATA);
                 totalDataSent.addAndGet(DATA.length);
-                }
-            }
-        
-        synchronized (m_dataServerSockets)
-            {
-            if (m_dataServerSockets.get() < NUM_REMOTE_HOSTS)
-                {
-                m_dataServerSockets.wait(6000);
-                }
-            }
-        
-        assertEquals(NUM_REMOTE_HOSTS, m_dataServerSockets.get());
-        
-        synchronized (totalDataReceived)
-            {
-            if (totalDataReceived.get() < totalDataSent.get())
-                {
-                totalDataReceived.wait(6000);
-                }
-            }
-
-        assertEquals(totalDataSent.get(), totalDataReceived.get());
-        
-        // The messages received should be TURN Send Indications that wrap
-        // TCP Frames.  They're already placed in the appropriate buckets for
-        // each remote host.  Now we just need to verify the data.
-        
-        for (final Map.Entry<InetSocketAddress, Collection<ByteBuffer>> entry : 
-            addressesToMessages.entrySet())
-            {
-            verifyData(entry.getValue());
             }
         }
-    
-    private void verifyData(final Collection<ByteBuffer> value)
-        {
-        m_log.debug("Combining "+value.size()+" messages");
+
+        synchronized (m_dataServerSockets) {
+            if (m_dataServerSockets.get() < NUM_REMOTE_HOSTS) {
+                m_dataServerSockets.wait(6000);
+            }
+        }
+
+        assertEquals(NUM_REMOTE_HOSTS, m_dataServerSockets.get());
+
+        synchronized (totalDataReceived) {
+            if (totalDataReceived.get() < totalDataSent.get()) {
+                totalDataReceived.wait(6000);
+            }
+        }
+
+        assertEquals("Did not receive all the data we sent",
+                totalDataSent.get(), totalDataReceived.get());
+
+        // The messages received should be TURN Send Indications that wrap
+        // TCP Frames. They're already placed in the appropriate buckets for
+        // each remote host. Now we just need to verify the data.
+
+        for (final Map.Entry<InetSocketAddress, Collection<ByteBuffer>> entry : 
+            addressesToMessages.entrySet()) {
+            verifyData(entry.getValue());
+        }
+    }
+
+    private void verifyData(final Collection<ByteBuffer> value) {
+        m_log.debug("Combining " + value.size() + " messages");
         final ByteBuffer combined = ByteBufferUtils.combine(value);
         final byte[] data = new byte[combined.capacity()];
         combined.get(data);
-        assertEquals("Lengths not equal", DATA.length*NUM_MESSAGES_PER_HOST, 
-            data.length);
-        
+        assertEquals("Lengths not equal", DATA.length * NUM_MESSAGES_PER_HOST,
+                data.length);
+
         // The server should have responded to all the messages that were sent,
         // so we'll have a bunch of messages combined here.
-        for (int i = 0; i < NUM_MESSAGES_PER_HOST; i++)
-            {
+        for (int i = 0; i < NUM_MESSAGES_PER_HOST; i++) {
             final int index = i * DATA.length;
-            final byte[] subArray = 
-                ArrayUtils.subarray(data, index, index+DATA.length);
+            final byte[] subArray = ArrayUtils.subarray(data, index, index
+                    + DATA.length);
             assertTrue("Data not equal", Arrays.equals(DATA, subArray));
             m_log.debug("One success!!!");
-            }
         }
+    }
 
-    private static byte[] createData()
-        {
+    private static byte[] createData() {
         // Just fill it with data with a clear order that can be screwed up
         // in the case of threading errors or anything else.
         final byte[] data = new byte[DATA_SIZE];
-        for (int i = 0; i < 127 ; i++)
-            {
+        for (int i = 0; i < 127; i++) {
             data[i] = (byte) (i % 127);
-            }
-        return data;
         }
+        return data;
+    }
     
-    private InetSocketAddress startThreadedServer() throws UnknownHostException
-        {
-        final InetSocketAddress serverAddress = 
-            new InetSocketAddress(NetworkUtils.getLocalHost(), 7896);
-        final Runnable runner = new Runnable()
-            {
-            public void run()
-                {
-                try
-                    {
+    private InetSocketAddress startThreadedServer() throws UnknownHostException {
+        final InetSocketAddress serverAddress = new InetSocketAddress(
+                NetworkUtils.getLocalHost(), 7896);
+        final Runnable runner = new Runnable() {
+            public void run() {
+                try {
                     startServer(serverAddress);
-                    }
-                catch (final Throwable t)
-                    {
-                    fail("Bad, bad server");
-                    }
+                } catch (final Throwable t) {
+                    fail("Bad, bad server: " + ThreadUtils.dumpStack());
                 }
-            };
-        final Thread turnThread = 
-            new Thread(runner, "TURN-Test-Data-Server-Thread");
+            }
+        };
+        final Thread turnThread = new Thread(runner,
+                "TURN-Test-Data-Server-Thread");
         turnThread.setDaemon(true);
         turnThread.start();
+        Thread.yield();
+        Thread.yield();
+        Thread.yield();
         return serverAddress;
-        }
+    }
 
-    private void startServer(final InetSocketAddress serverAddress) throws Exception
-        {
+    private void startServer(final InetSocketAddress serverAddress)
+            throws Exception {
         this.m_dataServerSocket = new ServerSocket(serverAddress.getPort());
-        synchronized (m_dataServerStarted)
-            {
+        synchronized (m_dataServerStarted) {
             m_dataServerStarted.set(true);
             m_dataServerStarted.notify();
-            }
-        
-        while(!this.m_dataServerSocket.isClosed())
-            {
+        }
+
+        while (!this.m_dataServerSocket.isClosed()) {
             final Socket client;
-            try
-                {
+            try {
                 client = m_dataServerSocket.accept();
                 m_dataServerSockets.incrementAndGet();
-                synchronized (m_dataServerSockets)
-                    {
+                synchronized (m_dataServerSockets) {
                     m_dataServerSockets.notify();
-                    }
                 }
-            catch (final SocketException se)
-                {
+            } catch (final SocketException se) {
                 m_log.debug("Socket closed");
                 assertTrue(m_stopDataServer.get());
                 // This will happen when the server socket's closed.
                 break;
-                }
-            m_log.debug("Got data socket number: "+m_dataServerSockets);
-            final Runnable serverSocketRunner = new Runnable()
-                {
-                public void run()
-                    {
-                    try
-                        {
+            }
+            m_log.debug("Got data socket number: " + m_dataServerSockets);
+            final Runnable serverSocketRunner = new Runnable() {
+                public void run() {
+                    try {
                         serveDataSocket(client);
-                        }
-                    catch (final Throwable t)
-                        {
+                    } catch (final Throwable t) {
                         m_socketServingFailed.set(true);
-                        }
                     }
-                };
-            final Thread serverSocketThread = 
-                new Thread(serverSocketRunner, "Server-Socket-Serving-Thread");
+                }
+            };
+            final Thread serverSocketThread = new Thread(serverSocketRunner,
+                    "Server-Socket-Serving-Thread");
             serverSocketThread.setDaemon(true);
             serverSocketThread.start();
-            }
         }
-    
-    private void serveDataSocket(final Socket client) throws IOException
-        {
-        final OutputStream os  = client.getOutputStream();
+    }
+
+    private void serveDataSocket(final Socket client) throws IOException {
+        final OutputStream os = client.getOutputStream();
         final InputStream is = client.getInputStream();
-        while (!m_stopDataServer.get())
-            {
+        while (!m_stopDataServer.get()) {
             final byte[] request = new byte[DATA_SIZE];
             final ByteBuffer buf = ByteBuffer.wrap(request);
             final ReadableByteChannel channel = Channels.newChannel(is);
-            while (buf.hasRemaining())
-                {
+            while (buf.hasRemaining()) {
                 channel.read(buf);
-                }
+            }
             m_dataOnServer.set(request);
-            
+
             m_dataServerMessagesReceived.incrementAndGet();
-            synchronized (m_dataServerMessagesReceived)
-                {
+            synchronized (m_dataServerMessagesReceived) {
                 m_dataServerMessagesReceived.notify();
-                }
-            m_log.debug("Client socket "+client.getPort()+" has received "+
-                m_dataServerMessagesReceived.get()+" messages...");
-            if (!Arrays.equals(DATA, request))
-                {
+            }
+            m_log.debug("Client socket " + client.getPort() + " has received "
+                    + m_dataServerMessagesReceived.get() + " messages...");
+            if (!Arrays.equals(DATA, request)) {
                 m_log.error("Bad data received on DATA server!!");
                 m_unexpectedDataOnServer.set(true);
-                }
-            else
-                {
+            } else {
                 m_dataServerMessagesValidated++;
-                m_log.debug("Data server has validated "+m_dataServerMessagesValidated+
-                    " messages...");
-                }
-            
+                m_log.debug("Data server has validated "
+                        + m_dataServerMessagesValidated + " messages...");
+            }
+
             // Now, write the response and make sure the remote host ultimately
             // gets the raw data back.
             os.write(DATA);
             os.flush();
             m_dataMessagesWrittenFromServer.incrementAndGet();
-            synchronized (m_dataMessagesWrittenFromServer)
-                {
+            synchronized (m_dataMessagesWrittenFromServer) {
                 m_dataMessagesWrittenFromServer.notify();
-                }
             }
         }
     }
+}
